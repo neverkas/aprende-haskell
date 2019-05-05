ff8439364809b1b855234651eb9f60cbb9fdeb3c
Emmanuel <emmanuel.lazarte.011@gmail.com>
2019-05-05 10:58:23 -0300

    Se agregaron primeros archivos de casos de prueba

--- src/Kars.hs
+++ src/Kars.hs
@@ -1,4 +1,145 @@
 module Kars where
-  
-    nombre :: String 
-    nombre = "carlitos"
+
+import Text.Show.Functions
+
+{- ##########################
+    FUNCIONES EXTRA 
+############################# -}
+
+duplicarVelocidad :: Float -> Float 
+duplicarVelocidad = (*2)
+
+aumentarEn :: Float -> Float -> Float
+aumentarEn n = (n+)
+
+aumentarUnQuintoDe :: Float -> Float -> Float
+aumentarUnQuintoDe n = aumentarEn (n*0.2) -- 0.20 es 1/5
+
+esVocal :: Char -> Bool
+esVocal 'a' = True
+esVocal 'e' = True 
+esVocal 'i' = True 
+esVocal 'o' = True 
+esVocal 'u' = True 
+esVocal _   = False
+
+cantidadVocales :: String -> Int
+cantidadVocales = length . filter (esVocal) 
+
+hayCantidadVocalesMayorA :: Int -> String -> Bool
+hayCantidadVocalesMayorA n = (>n) . cantidadVocales
+
+hayCantidadVocalesEntre :: Int -> Int -> String -> Bool
+hayCantidadVocalesEntre desde hasta palabra = 
+    cantidadVocales palabra >= desde && cantidadVocales palabra <= hasta
+
+usarTruco :: Auto -> Auto
+usarTruco auto = (truco auto) auto
+
+{- ##########################
+    MODELADO 
+############################# -}
+
+type Participante = Auto -> Auto
+type Truco = Auto -> Auto 
+
+data Auto = Auto{
+    nombre :: String,
+    nivelDeNafta :: Float, 
+    velocidad :: Float,
+    nombreDeEnamorade :: String,
+    truco :: Truco
+} deriving (Show)
+
+
+{- ##########################
+    MODELADO TRUCOS
+############################# -}
+
+deReversa :: Truco
+deReversa auto = auto {
+    nivelDeNafta = ((aumentarUnQuintoDe 1000) . nivelDeNafta) auto 
+}
+
+impresionar :: Truco
+impresionar auto = auto {
+    velocidad = (duplicarVelocidad . velocidad) auto
+}
+
+nitro :: Truco
+nitro auto = auto{
+    velocidad = ((aumentarEn 15) . velocidad) auto
+}
+
+fingirAmor :: String -> Truco
+fingirAmor nuevoNombreDeEnamorade auto = auto{
+    nombreDeEnamorade = nuevoNombreDeEnamorade
+}
+
+incrementarVelocidad :: Participante
+incrementarVelocidad auto 
+    | ((hayCantidadVocalesEntre 1 2) . nombreDeEnamorade) auto = auto {
+        velocidad = (aumentarEn 15 . velocidad) auto
+        }
+    | ((hayCantidadVocalesEntre 3 4) . nombreDeEnamorade) auto = auto {
+        velocidad = (aumentarEn 20 . velocidad) auto
+        }
+    | ((hayCantidadVocalesMayorA 4) . nombreDeEnamorade) auto = auto {
+        velocidad = (aumentarEn 30 . velocidad) auto
+        }
+    | otherwise = auto
+
+puedeRealizarTruco :: Auto -> Bool
+puedeRealizarTruco (Auto _ nivelDeNafta velocidad _ _) = 
+    nivelDeNafta > 0 && velocidad < 100
+
+comboLoco :: Truco
+comboLoco = deReversa . nitro
+
+turbo :: Truco
+turbo (Auto nombre nivelDeNafta velocidad nombreDeEnamorade truco) =
+    Auto nombre 0 (aumentarEn velocidad (nivelDeNafta*10)) nombreDeEnamorade truco                
+
+queTrucazo :: String -> Participante
+queTrucazo nombre = (incrementarVelocidad . fingirAmor nombre)
+
+{- ##########################
+    MODELADO AUTOS
+############################# -}
+
+rochaMcQueen :: Auto
+rochaMcQueen = Auto {
+    nombre = "RochaMcQueen",
+    nivelDeNafta = 300,
+    velocidad = 0,
+    nombreDeEnamorade = "Ronco",
+    truco = deReversa
+}
+
+biankerr :: Auto
+biankerr = Auto {
+    nombre = "Biankerr",
+    nivelDeNafta = 500,
+    velocidad = 20,
+    nombreDeEnamorade = "Tinch",
+    truco = impresionar
+}
+
+gushtav :: Auto
+gushtav = Auto {
+    nombre = "Gushtav",
+    nivelDeNafta = 200,
+    velocidad = 130,
+    nombreDeEnamorade = "PetiLaLinda",
+    truco = nitro
+}
+
+rodra :: Auto
+rodra = Auto {
+    nombre = "Rodra",
+    nivelDeNafta = 0,
+    velocidad = 50,
+    nombreDeEnamorade = "Taisa",
+    truco = fingirAmor "Petra"
+}
+                    
--- test/KarsSpec.hs
+++ test/KarsSpec.hs
@@ -1,13 +1,79 @@
 module KarsSpec (spec) where
 
 import Test.Hspec
+import Kars
 
 spec :: Spec
 spec = do
-{-
-main :: IO ()
-main = hspec spec
--}
-    describe "Probando sumas" $ do
-        it "1+1 es 2" $ do
-            1+1 `shouldBe` 2
+    describe "Consultar la nafta de RochaMcQueen luego de realizar su truco" $ do 
+        it "nivelNafalta es 500" $ do
+            (nivelDeNafta . truco rochaMcQueen) rochaMcQueen `shouldBe` 500
+
+    describe "Consultar la velocidad de Biankerr luego de realizar su truco" $ do 
+        it "velocidad es 40" $ do 
+            (velocidad . usarTruco) biankerr `shouldBe` 40
+
+    describe "Consultar la velocidad de Gushtav luego de realizar su truco" $ do 
+        it "velocidad es 145" $ do 
+            (velocidad . usarTruco) gushtav `shouldBe` 145
+
+    describe "Consultar la enamorada de Rodra cuando elige a Petra." $ do 
+        it "velocidad es 40" $ do 
+            (nombreDeEnamorade . fingirAmor "Petra") rodra `shouldBe` "Petra"
+            
+    describe "Consultar la velocidad de RochaMcQueen luego de incrementar su velocidad" $ do 
+        it "velocidad es 15" $ do 
+            (velocidad . incrementarVelocidad) rochaMcQueen `shouldBe` 15
+
+    describe "Consultar la velocidad de Biankerr luego de incrementar su velocidad" $ do 
+        it "velocidad es 35" $ do 
+            (velocidad . incrementarVelocidad) biankerr `shouldBe` 35
+
+    describe "Consultar la velocidad de Gushtav luego de incrementar su velocidad" $ do 
+        it "velocidad es 160" $ do 
+            (velocidad . incrementarVelocidad) gushtav `shouldBe` 160
+
+    describe "Consultar la velocidad de Rodra luego de incrementar su velocidad" $ do 
+        it "velocidad es 70" $ do 
+            (velocidad . incrementarVelocidad) rodra `shouldBe` 70
+
+    describe "Consultar si RochaMcQueen puede usar su truco" $ do 
+        it "es True" $ do 
+            puedeRealizarTruco rochaMcQueen `shouldBe` True
+
+    describe "Consultar si Gushtav puede usar su truco" $ do 
+        it "es False" $ do 
+            puedeRealizarTruco gushtav `shouldBe` False
+    
+    describe "Consultar si Rodra puede usar su truco" $ do 
+        it "es False" $ do 
+            puedeRealizarTruco rodra `shouldBe` False
+
+    -- # Punto 3.4
+    describe "Consultar la nafta de Rocha luego de realizar comboLoco" $ do 
+        it "nivelDeNafta es 500" $ do 
+            (nivelDeNafta . comboLoco) rochaMcQueen `shouldBe` 500
+
+    describe "Consultar la velocidad de Rocha luego de realizar comboLoco" $ do 
+        it "velocidad es 15" $ do 
+            (velocidad . comboLoco) rochaMcQueen `shouldBe` 15        
+
+    describe "Consultar la velocidad de Rodra luego de utilizar que Trucazo cambiando su enamorada a Murcielago" $ do 
+        it "velocidad es 80" $ do 
+            (velocidad . queTrucazo "Murcielago") rodra  `shouldBe` 80
+
+    describe "Consultar la velocidad de Gushtav luego de utilizar turbo" $ do 
+        it "velocidad es 2130" $ do 
+            (velocidad . turbo) gushtav `shouldBe` 2130
+        
+    describe "Consultar la nafta de Gushtav luego de utilizar turbo" $ do 
+        it "nivelDeNafta es 0" $ do 
+            (nivelDeNafta . turbo) gushtav `shouldBe` 0
+
+    describe "Consultar la velocidad de Rodra luego de utilizar turbo" $ do 
+        it "velocidad es 50" $ do 
+            (velocidad . turbo) rodra `shouldBe` 50
+
+    describe "Consultar la nafta de Rodra luego de utilizar turbo" $ do 
+        it "nivelDeNafta es 0" $ do 
+            (nivelDeNafta . turbo) rodra `shouldBe` 0
