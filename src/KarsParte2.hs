15e44380c2bc4f0fa908c2ad3f8e63d1bedbb9ad
Emmanuel <emmanuel.lazarte.011@gmail.com>
2019-05-21 20:28:54 -0300

    Probando nueva entrega

---
+++ src/KarsParte2.hs
@@ -0,0 +1,340 @@
+module KarsParte2 where
+
+    import Text.Show.Functions
+    
+    {- ##########################
+        FUNCIONES EXTRA 
+    ############################# -}
+    
+    largoPista = 1000
+    
+    duplicarVelocidad :: Float -> Float 
+    duplicarVelocidad = (*2)
+    
+    aumentarEn :: Float -> Float -> Float
+    aumentarEn numero = (numero+)
+    
+    aumentarUnQuintoDe :: Float -> Float -> Float
+    aumentarUnQuintoDe numero = aumentarEn (numero*0.2) -- 0.20 es 1/5
+    
+    esVocal :: Char -> Bool
+    esVocal 'a' = True
+    esVocal 'e' = True 
+    esVocal 'i' = True 
+    esVocal 'o' = True 
+    esVocal 'u' = True 
+    esVocal _   = False
+    
+    cantidadVocales :: String -> Int
+    cantidadVocales = length . filter (esVocal) 
+    
+    hayCantidadVocalesMayorA :: Int -> String -> Bool
+    hayCantidadVocalesMayorA numero = (>numero) . cantidadVocales
+    
+    hayCantidadVocalesEntre :: Int -> Int -> String -> Bool
+    hayCantidadVocalesEntre desde hasta palabra = 
+        cantidadVocales palabra >= desde && cantidadVocales palabra <= hasta
+    
+    tieneNafta :: Auto -> Bool 
+    tieneNafta = (>0) . nivelDeNafta
+    
+    usarTruco :: Auto -> Auto
+    usarTruco auto = (truco auto) auto
+    
+    {- ##########################
+        MODELADO 
+    ############################# -}
+    
+    type Participante = Auto -> Auto
+    type Truco = Auto -> Auto 
+    
+    data Auto = Auto {
+        nombre :: String,
+        nivelDeNafta :: Float, 
+        velocidad :: Float,
+        nombreDeEnamorade :: String,
+        truco :: Truco
+    } deriving (Show)
+    
+    
+    {- ##########################
+        MODELADO TRUCOS
+    ############################# -}
+    
+    --De reversa esta más adelante-- 
+    
+    impresionar :: Truco
+    impresionar auto = auto {
+        velocidad = (duplicarVelocidad . velocidad) auto
+    }
+    
+    nitro :: Truco
+    nitro auto = auto {
+        velocidad = (aumentarEn 15 . velocidad) auto
+    }
+    
+    fingirAmor :: String -> Truco
+    fingirAmor nuevoNombreDeEnamorade auto = auto{
+        nombreDeEnamorade = nuevoNombreDeEnamorade
+    }
+    
+    incrementarVelocidad :: Participante
+    incrementarVelocidad auto 
+        | ((hayCantidadVocalesEntre 1 2) . nombreDeEnamorade) auto = auto {
+            velocidad = (aumentarEn 15 . velocidad) auto
+            }
+        | ((hayCantidadVocalesEntre 3 4) . nombreDeEnamorade) auto = auto {
+            velocidad = (aumentarEn 20 . velocidad) auto
+            }
+        | ((hayCantidadVocalesMayorA 4) . nombreDeEnamorade) auto = auto {
+            velocidad = (aumentarEn 30 . velocidad) auto
+            }
+        | otherwise = auto
+    
+    puedeRealizarTruco :: Auto -> Bool
+    puedeRealizarTruco auto = velocidad auto < 100 && tieneNafta auto
+        
+    comboLoco :: Truco
+    comboLoco = deReversa . nitro
+    
+    turbo :: Truco
+    turbo (Auto nombre nivelDeNafta velocidad nombreDeEnamorade truco) =
+        Auto nombre 0 (aumentarEn velocidad (nivelDeNafta*10)) nombreDeEnamorade truco                
+    
+    queTrucazo :: String -> Participante
+    queTrucazo nombre = incrementarVelocidad . fingirAmor nombre
+    
+    {- ##########################
+        MODELADO AUTOS
+    ############################# -}
+    
+    rochaMcQueen :: Auto
+    rochaMcQueen = Auto {
+        nombre = "RochaMcQueen",
+        nivelDeNafta = 300,
+        velocidad = 0,
+        nombreDeEnamorade = "Ronco",
+        truco = deReversa
+    }
+    
+    biankerr :: Auto
+    biankerr = Auto {
+        nombre = "Biankerr",
+        nivelDeNafta = 500,
+        velocidad = 20,
+        nombreDeEnamorade = "Tinch",
+        truco = impresionar
+    }
+    
+    gushtav :: Auto
+    gushtav = Auto {
+        nombre = "Gushtav",
+        nivelDeNafta = 200,
+        velocidad = 130,
+        nombreDeEnamorade = "PetiLaLinda",
+        truco = nitro
+    }
+    
+    rodra :: Auto
+    rodra = Auto {
+        nombre = "Rodra",
+        nivelDeNafta = 0,
+        velocidad = 50,
+        nombreDeEnamorade = "Taisa",
+        truco = fingirAmor "Petra"
+    }
+    
+    ----------------
+    ----------------
+    --- PARTE 2 ----
+    ----------------
+    ----------------
+    
+    -------------------------
+    --- MODELADO CARRERA ----
+    -------------------------
+    
+    type Trampa = Carrera -> Carrera
+    
+    data Carrera = Carrera {
+        vueltasACorrer :: Int,
+        longitudPista :: Float,
+        integrantesPublico :: [String],
+        trampa :: Trampa,
+        participantes :: [Auto]
+    } deriving (Show)
+    
+    potreroFunes = Carrera {
+        vueltasACorrer = 3,
+        longitudPista = 5.0,
+        integrantesPublico = ["Ronco", "Tinch", "Dodain"],
+        trampa = sacarAlPistero,
+        participantes = [rochaMcQueen, biankerr, gushtav, rodra]
+    } 
+    
+    -- DE REVERSA --
+    
+    deReversa :: Truco
+    deReversa auto = auto {
+        -- en la entrega anterior se utilizaba como referencia el largo de la pista
+        --nivelDeNafta = (aumentarUnQuintoDe largoPista . nivelDeNafta) auto         
+        nivelDeNafta = (aumentarUnQuintoDe (velocidad auto) . nivelDeNafta) auto
+    }
+    
+    -------------
+    -- TRAMPAS -- 
+    -------------
+    
+    sacarAlPistero :: Trampa
+    sacarAlPistero carrera = carrera {
+        participantes = drop 1 $ participantes carrera
+    }
+    
+    lluvia :: Trampa
+    lluvia carrera = carrera {
+      participantes = map disminuirVelocidadEn10 $ participantes carrera
+    }
+    
+    neutralizarTrucos :: Trampa
+    neutralizarTrucos carrera = carrera {
+        participantes = map inutilizarAuto $ participantes carrera
+    }
+    
+    pocaReserva :: Trampa
+    pocaReserva carrera = carrera {
+        participantes = filter ((>= 30) . nivelDeNafta) $ participantes carrera
+    }
+    
+    podio :: Trampa
+    podio carrera = carrera {
+        participantes = take 3 $ participantes carrera
+    }
+    
+    -- Funciones Auxiliares Del Punto 2 -- 
+    
+    disminuirVelocidadEn10 :: Truco
+    disminuirVelocidadEn10 auto = auto {
+        velocidad = aumentarEn (-10) . velocidad $ auto
+    }
+    
+    inutilizarAuto :: Auto -> Auto
+    inutilizarAuto auto = auto {
+        truco = inutilidad
+    }
+    
+    inutilidad :: Truco 
+    inutilidad auto = auto
+    {-
+    inutilidad (Auto nombre nivelDeNafta velocidad nombreDeEnamorade truco) =
+        Auto nombre nivelDeNafta velocidad nombreDeEnamorade truco     
+    -}
+    
+    -----------------
+    -- A CORRER !! -- 
+    -----------------
+    
+    -- ITEM 1 --
+    
+    darVuelta :: Carrera -> Carrera
+    darVuelta carrera = (trampa carrera) . hacerTrucoSegunEnamorade . restarCombustibleATodos $ carrera
+    
+    -- Funciones Auxiliares Del Punto 3.1 -- 
+    
+    restarCombustibleATodos :: Carrera -> Carrera
+    restarCombustibleATodos carrera = carrera {
+        participantes = map (restarCombustible $ longitudPista carrera) $ participantes carrera
+    }
+    
+    calcularFormula :: Float -> Auto -> Float
+    --calcularFormula kmDeLaPista = ((/kmDeLaPista) . (*10) . velocidad)
+    calcularFormula kmDeLaPista = (kmDeLaPista / 10 *) . velocidad
+
+    restaEntrePositivos :: Float -> Float -> Float
+    restaEntrePositivos numero1 numero2 = max 0 $ numero1 - numero2
+
+    restarCombustible :: Float -> Auto -> Auto 
+    restarCombustible kmDeLaPista auto = auto {
+        --nivelDeNafta = max 0 ((nivelDeNafta auto) - kmDeLaPista  / 10 * (velocidad auto))                
+        nivelDeNafta = restaEntrePositivos (nivelDeNafta auto) (calcularFormula kmDeLaPista auto)
+    }
+    
+    hacerTrucoSegunEnamorade :: Carrera -> Carrera
+    hacerTrucoSegunEnamorade carrera =  carrera {
+        participantes = map (hacerSegunEnamorade $ integrantesPublico carrera) $ participantes carrera 
+    }
+    
+    hacerSegunEnamorade :: [String] -> Auto -> Auto
+    hacerSegunEnamorade enamorades auto 
+      | estaEnamoradeEnElPublico auto enamorades = hacerTrucoDelAuto auto
+      -- | estaEnamoradeEnElPublico enamorades auto = hacerTrucoDelAuto auto
+      | otherwise = auto
+    
+    hacerTrucoDelAuto :: Auto -> Auto
+    hacerTrucoDelAuto auto = truco auto $ auto
+    
+    {-
+    estaEnamoradeEnElPublico :: [String] -> Auto -> Bool
+    estaEnamoradeEnElPublico enamorades auto = elem (nombreDeEnamorade auto) enamorades
+    -}
+    estaEnamoradeEnElPublico :: Auto -> [String] -> Bool
+    estaEnamoradeEnElPublico auto = elem (nombreDeEnamorade auto)
+    
+    -- ITEM 2 -- 
+    
+    correrCarrera :: Carrera -> Carrera
+    correrCarrera carrera = (!!) (aplicarDarVuelta carrera) $ vueltasACorrer carrera 
+    
+    -- Funciones Auxiliares Del Punto 3.2 -- 
+    
+    aplicarDarVuelta :: Carrera -> [Carrera]
+    aplicarDarVuelta carrera = autosDespuesDeLaVuelta carrera $ iterate darVuelta carrera
+    
+    --autosDespuesDeLaVuelta :: Carrera -> [a] -> [a]
+    autosDespuesDeLaVuelta carrera = take (vueltasACorrer carrera + 1)
+    
+    -- sin delegar -- 
+    
+    correrCarrera' :: Carrera -> Carrera
+    correrCarrera' carrera = (!!) (take (vueltasACorrer carrera + 1 ) $ iterate darVuelta carrera) (vueltasACorrer carrera)  
+    
+    -------------------
+    -- QUIEN GANA ?? --
+    -------------------
+    
+    quienGana :: Carrera -> Auto
+    quienGana carrera = elMasRapidoDeTodos (participantes $ correrCarrera carrera) 
+    
+    -- Funciones Auxiliares Del Punto 4 -- 
+    
+    elMasRapidoDeTodos :: [Auto] -> Auto
+    elMasRapidoDeTodos = foldr1 elMasRapidoEntre2   
+    
+    elMasRapidoEntre2 :: Auto -> Auto -> Auto 
+    elMasRapidoEntre2 auto1 auto2 
+     | velocidad auto1 > velocidad auto2 = auto1 
+     | otherwise = auto2 
+    
+    -------------------
+    -- EL GRAN TRUCO --
+    -------------------
+    
+    trucos = [nitro, deReversa, impresionar]
+    
+    elGranTruco :: Auto -> [Truco] -> Auto 
+    elGranTruco auto = foldl aplicarTruco auto  
+    
+    aplicarTruco :: Auto -> Truco -> Auto
+    aplicarTruco auto unTruco = unTruco $ auto
+    
+    -------------------
+    --CASOS DE PRUEBA--
+    -------------------
+    
+    {-
+    
+    velocidad $ last $ participantes $ lluvia prueba
+    velocidad . last . participantes . lluvia $ prueba
+    
+    > 40 
+    
+    -}
---
+++ test/KarsParte2Spec.hs
@@ -0,0 +1,90 @@
+module KarsParte2Spec (spec) where
+
+import Test.Hspec
+--import KarsParte2
+import KarsParte2Original
+
+spec :: Spec
+spec = do
+    {-
+    # PUNTO 3.0
+    -}
+    describe "Consultar la nafta de rochaMcQueen luego de hacer su truco favorito" $ do 
+        it "nivelNafalta es 300" $ do
+            (nivelDeNafta . truco rochaMcQueen) rochaMcQueen `shouldBe` 300
+
+    describe "Consultar la nafta de rodra tras deReversa" $ do 
+        it "nivelNafalta es 10" $ do
+            (nivelDeNafta . deReversa) rodra `shouldBe` 10
+
+    {-
+    # PUNTO 3.2
+    -}
+    describe "Consultar la cantidad de participantes luego de sacarAlPistero en potreroFunes" $ do 
+        it "la cantidad de participantes es 3" $ do
+            (length . participantes . sacarAlPistero) potreroFunes `shouldBe` 3
+    
+    describe "rochaMcQueen ya no participa en potreroFunes tras sacarAlPistero" $ do 
+        it "debe ser True" $ do
+            (not . elem "RochaMcQueen" $ map (nombre) ((participantes.sacarAlPistero)  potreroFunes)) `shouldBe` True
+
+    describe "Consultar la cantidad de participantes luego de pocaReserva en potreroFunes" $ do 
+        it "la cantidad de participantes es 3" $ do
+            (length . participantes . pocaReserva) potreroFunes `shouldBe` 3
+    
+    describe "El participante llamado “rodra” ya no debería estar entre los participantes de potreroFunes luego de aplicar pocaReserva" $ do 
+        it "debe ser True" $ do
+            (not . elem "Rodra" $ map (nombre) ((participantes.pocaReserva)  potreroFunes)) `shouldBe` True
+            
+    describe "Consultar la cantidad de participantes luego de aplicar podio en potreroFunes" $ do 
+        it "la cantidad de participantes es 3" $ do
+            (length . participantes . podio) potreroFunes `shouldBe` 3
+
+    describe "Consultar la velocidad del último participante de potreroFunes (rodra) luego de la lluvia" $ do 
+        it "la velocidad es 40" $ do
+            (velocidad . last . participantes . lluvia) potreroFunes `shouldBe` 40
+        
+    {-
+    # PUNTO 3.3
+    -}
+    describe "Consultar el nivel de nafta del primer participante (biankerr porque rochaMcQueen quedó afuera)  luego de dar una vuelta en potreroFunes" $ do 
+        it "el nivel de nafta es 490" $ do
+            (nivelDeNafta.head.participantes.darVuelta) potreroFunes `shouldBe` 490
+        
+    describe "Consultar la velocidad del primer participante (biankerr porque rochaMcQueen quedó afuera) luego de dar una vuelta en potreroFunes" $ do 
+        it "la velocidad es 40" $ do
+            (velocidad.head.participantes.darVuelta) potreroFunes `shouldBe` 40
+        
+    describe "Consultar la cantidad de participantes tras dar dos vueltas en potrero funes" $ do 
+        it "la cantidad de participantes es 2" $ do
+            (length.participantes.darVuelta.darVuelta) potreroFunes `shouldBe` 2
+        
+    describe "Luego de 2 vueltas en potreroFunes, consultar el nivelDeNafta del primer participante (gushtav)" $ do 
+        it "la velocidad es 70" $ do
+            (nivelDeNafta.head.participantes.darVuelta.darVuelta) potreroFunes `shouldBe` 70
+
+    describe "Rodra debe ser el único participante luego de correr la carrera de potreroFunes" $ do 
+        it "debe ser True" $ do
+            --(participantes.correrCarrera) potreroFunes `shouldBe` 70
+            (elem "Rodra" $ map nombre ((participantes.correrCarrera) potreroFunes)) `shouldBe` True
+
+    {-
+    # PUNTO 3.4
+    -}
+    describe "Consultar el ganador de potreroFunes" $ do 
+        it "el ganador es rodra" $ do
+            (nombre.quienGana $ potreroFunes) `shouldBe` "Rodra"
+
+    {-
+    # PUNTO 3.5
+    -}
+    describe "Consultar la velocidad de rodra tras realizar elGranTruco con nitro, deReversa e impresionar" $ do 
+        it "la velocidad es 130" $ do
+            (velocidad $ elGranTruco rodra [nitro, deReversa, impresionar]) `shouldBe` 130
+            
+    describe "Consultar el nivelDeNafta de rodra tras realizar elGranTruco con nitro, deReversa e impresiona" $ do 
+        it "la velocidad es 13" $ do
+            (nivelDeNafta $ elGranTruco rodra [nitro, deReversa, impresionar]) `shouldBe` 13
+        
+
+    
